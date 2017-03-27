54fdb26d63a971d49b0e07c433b74cf46a0a4098
Florian Herrmann <florian.herrmann@m2m-systems.de>
2017-03-27 13:10:22 +0200

    Integrated LSM303

--- src/kk-mgh-flexikurv.ino
+++ src/kk-mgh-flexikurv.ino
@@ -1,14 +1,22 @@
 #include <TinyWireM.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_LSM303_U.h>
 
 #define LED_LEFT 0
 #define LED_MIDDLE 1
 #define LED_RIGHT 2
 
 #define USE_SERIAL
+#define USE_LSM303
 
 #ifdef USE_SERIAL
 	#include <SoftwareSerial.h>
 	SoftwareSerial softSerial( 9 , 10 );
+#endif
+
+#ifdef USE_LSM303
+	#include <Adafruit_LSM303_U.h>
+	Adafruit_LSM303_Accel_Unified lsm303_accel = Adafruit_LSM303_Accel_Unified(54321);
 #endif
 
 void setup() {
@@ -26,15 +34,41 @@ void setup() {
 		softSerial.println( "FlexiKurv Prototype" );
 	#endif
 
+	#ifdef USE_LSM303
+		#ifdef USE_SERIAL
+			softSerial.println( "Use LSM303 Accelerometer" );
+		#endif
+
+		if( !lsm303_accel.begin() ) {
+			#ifdef USE_SERIAL
+				softSerial.println("No LSM303 detected!");
+			#endif
+			while( true );
+		}
+	#endif
+
 }
 
 // the loop function runs over and over again forever
 void loop() {
 
-	#ifdef USE_SERIAL
-		softSerial.print( "Hello World!" );
-		delay( 1000 );
-  	#endif
+	#ifdef USE_LSM303
+		sensors_event_t event;
+    	lsm303_accel.getEvent(&event);
+
+    	#ifdef USE_SERIAL
+			softSerial.print( "X Raw: " ); softSerial.print( lsm303_accel.raw.x ); softSerial.print( "  " );
+			softSerial.print( "Y Raw: " ); softSerial.print( lsm303_accel.raw.y ); softSerial.print( "  " );
+			softSerial.print( "Z Raw: " ); softSerial.print( lsm303_accel.raw.z ); softSerial.println( "" );
+
+	    	softSerial.print( " -- X: " ); softSerial.print( event.acceleration.x ); softSerial.print( "  " );
+	    	softSerial.print( "Y: " ); softSerial.print( event.acceleration.y ); softSerial.print( "  " );
+	    	softSerial.print( "Z: " ); softSerial.print( event.acceleration.z ); softSerial.print( "  " );
+			softSerial.println( "m/s^2 " );
+
+			delay( 500 );
+		#endif
+	#endif
 
 }
 
