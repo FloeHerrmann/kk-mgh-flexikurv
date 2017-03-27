19c001c8003655117cb2291b257d3201dde3ce3b
Florian Herrmann <florian.herrmann@m2m-systems.de>
2017-03-27 13:47:54 +0200

    Integrated L3GD20
    Integrated L3GD20 Gyroscope

--- lib/Adafruit_LSM303DLHC/Adafruit_LSM303.cpp
+++ lib/Adafruit_LSM303DLHC/Adafruit_LSM303.cpp
@@ -2,7 +2,7 @@
 #include <TinyWireM.h>
 #include <limits.h>
 
-#include "Adafruit_LSM303_U.h"
+#include "Adafruit_LSM303.h"
 
 static float _lsm303Accel_MG_LSB = 0.001F;   // 1, 2, 4 or 12 mg per lsb
 
--- src/kk-mgh-flexikurv.ino
+++ src/kk-mgh-flexikurv.ino
@@ -1,13 +1,12 @@
 #include <TinyWireM.h>
-#include <Adafruit_Sensor.h>
-#include <Adafruit_LSM303_U.h>
 
 #define LED_LEFT 0
 #define LED_MIDDLE 1
 #define LED_RIGHT 2
 
 #define USE_SERIAL
-#define USE_LSM303
+//#define USE_LSM303
+#define USE_L3GD20
 
 #ifdef USE_SERIAL
 	#include <SoftwareSerial.h>
@@ -15,8 +14,15 @@
 #endif
 
 #ifdef USE_LSM303
-	#include <Adafruit_LSM303_U.h>
+	#include <Adafruit_Sensor.h>
+	#include <Adafruit_LSM303.h>
 	Adafruit_LSM303_Accel_Unified lsm303_accel = Adafruit_LSM303_Accel_Unified(54321);
+#endif
+
+#ifdef USE_L3GD20
+	#include <Adafruit_Sensor.h>
+	#include <Adafruit_L3GD20.h>
+	Adafruit_L3GD20_Unified l3gd20_gyro = Adafruit_L3GD20_Unified( 20 );
 #endif
 
 void setup() {
@@ -47,13 +53,31 @@ void setup() {
 		}
 	#endif
 
+	#ifdef USE_L3GD20
+		#ifdef USE_SERIAL
+			softSerial.println( "Use L3GD20 Gyroscope" );
+		#endif
+
+		l3gd20_gyro.enableAutoRange(true);
+		if( !l3gd20_gyro.begin() ) {
+			#ifdef USE_SERIAL
+				softSerial.println("No L3GD20 detected!");
+			#endif
+			while( true );
+		}
+	#endif
+
 }
 
 // the loop function runs over and over again forever
 void loop() {
 
-	#ifdef USE_LSM303
+	#if defined( USE_LSM303 ) || defined( USE_L3GD20 )
 		sensors_event_t event;
+	#endif
+
+	#ifdef USE_LSM303
+
     	lsm303_accel.getEvent(&event);
 
     	#ifdef USE_SERIAL
@@ -65,6 +89,19 @@ void loop() {
 	    	softSerial.print( "Y: " ); softSerial.print( event.acceleration.y ); softSerial.print( "  " );
 	    	softSerial.print( "Z: " ); softSerial.print( event.acceleration.z ); softSerial.print( "  " );
 			softSerial.println( "m/s^2 " );
+
+			delay( 500 );
+		#endif
+	#endif
+
+	#ifdef USE_L3GD20
+    	l3gd20_gyro.getEvent(&event);
+
+    	#ifdef USE_SERIAL
+			softSerial.print( "X: " ); softSerial.print( event.gyro.x ); softSerial.print( "  " );
+  			softSerial.print( "Y: " ); softSerial.print( event.gyro.y ); softSerial.print( "  " );
+  			softSerial.print( "Z: " ); softSerial.print( event.gyro.z ); softSerial.print( "  " );
+  			softSerial.println("rad/s ");
 
 			delay( 500 );
 		#endif
